d8deaa495539b495a8aeedddbd4985a9cab9b81d
jaanisfehling <jaanisfehling@gmail.com>
2023-05-27 00:32:25 +0200

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'std-blue': '#123456'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
