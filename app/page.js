cabcd4d694ee7de23f07c1e4d49e5f07780c3ab6
jaanisfehling <jaanisfehling@gmail.com>
2023-05-27 20:17:47 +0200

    navbar

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
 export default function Home() {
     return (
         <main className="flex min-h-screen bg-white">
-            <div className="flex bg-std-blue sticky h-14 w-full items-center">
-                <div className="mx-8">
-                    <span className="text-2xl text-white ">Urza</span>
-                </div>
+            <div className="flex px-8 justify-between bg-std-blue sticky h-14 w-full items-center">
+                <span className="text-2xl text-white">Urza</span>
+                <button className="text-2xl text-white">Urza</button>
             </div>
         </main>
     )
