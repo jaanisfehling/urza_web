7e0c3f0502c38a35460231454f40f33d73313b52
jaanisfehling <jaanisfehling@gmail.com>
2023-05-31 14:54:18 +0200

    route and links to login route

---
+++ app/login/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Login() {
+    return (
+        <main className="flex min-h-screen bg-white flex-col">
+            <div className="flex-col items-center m-auto space-y-5">
+                <h1>
+                    Hello world
+                </h1>
+            </div>
+        </main>
+    )
+}
---
+++ components/navbar.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export function Navbar() {
+    return (
+        <div className="flex px-4 justify-between bg-white sticky h-14 w-full items-center border-std-blue border-b-2">
+            <span className="text-2xl text-std-blue">Urza</span>
+            <div className="flex space-x-4">
+                <Link href="/login" className="flex h-8 px-2.5 m-2 bg-std-blue hover:bg-std-blue-hover rounded-md items-center text-white font-medium text-base" type="button">
+                    Login
+                </Link>
+                <Link href="/login" className="flex h-8 px-2.5 m-2 bg-std-blue hover:bg-std-blue-hover rounded-md items-center text-white font-medium text-base" type="button">
+                    Sign Up
+                </Link>
+            </div>
+        </div>
+    )
+}
