9f3f09b23892e2e25057f87cb77d2224bb124b52
jaanisfehling <jaanisfehling@gmail.com>
2023-06-05 23:36:20 +0200

    remove signup component, refactor css

--- app/login/page.js
+++ app/login/page.js
@@ -2,12 +2,8 @@ import Link from "next/link";
 
 export default function Login() {
     return (
-        <main className="flex min-h-screen bg-white flex-col">
-            <div className="flex-col items-center m-auto space-y-5">
-                <h1>
-                    Hello world
-                </h1>
-            </div>
-        </main>
+        <h1>
+            Hello world
+        </h1>
     )
 }
--- app/page.js
+++ app/page.js
@@ -3,10 +3,10 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <main className="flex flex-col min-h-screen bg-white">
+        <main className="min-h-screen bg-white flex flex-col">
             <Navbar></Navbar>
-            <div className="flex-col items-center m-auto space-y-5">
-                <h1 className="py-4 px-0.5">
+            <div className="flex flex-col items-center m-auto space-y-4">
+                <h1 className="">
                     Hello world
                 </h1>
                 <Link href="/signup" className="text-white bg-std-blue hover:bg-std-blue-hover font-medium rounded-sm text-base px-4 py-2.5">
