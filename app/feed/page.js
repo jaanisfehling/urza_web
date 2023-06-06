462a8b9425ebfdcede0f65508dbc5414c823efb4
jaanisfehling <jaanisfehling@gmail.com>
2023-06-06 14:03:25 +0200

    feed useEffect hook

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,6 +1,8 @@
+"use client"
+
 import {baseUrl} from "@/consts";
 import {useRouter} from "next/navigation";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export default function Feed() {
     const [data, setData] = useState(null);
@@ -9,24 +11,29 @@ export default function Feed() {
 
     const router = useRouter();
 
-    fetch(baseUrl + "news/article/", {
-        method: "GET",
-        headers: {"Content-Type": "application/json"}
-    }).then(function(response) {
-        if (!response.ok) {
-            return response.json();
+    useEffect(() => {
+        const fetchInitialArticles = async () => {
+            fetch(baseUrl + "news/article/", {
+                method: "GET",
+                headers: {"Content-Type": "application/json"},
+                cache: "no-store"
+            }).then(function(response) {
+                if (!response.ok) {
+                    return response.json();
+                }
+                setIsLoading(false);
+                setData(response.json());
+                setErrors([]);
+            }).then(function(result) {
+                setErrors(result);
+            });
         }
-    }).then(function(result) {
-        let msgs = [];
-        for (const [key, value] of Object.entries(result)) {
-            msgs.push(...value);
-        }
-        setErrors(msgs);
-    });
+        fetchInitialArticles();
+    }, []);
+
 
     return (
         <main className="bg-white min-h-screen">
-            Hello World
         </main>
     )
 }
