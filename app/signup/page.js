8e0a66addaa15837f70f767f3ff44e64739ffa35
jaanisfehling <jaanisfehling@gmail.com>
2023-06-10 16:05:47 +0200

    reroute to welcome page after succesful signup

--- app/signup/page.js
+++ app/signup/page.js
@@ -4,14 +4,13 @@ import {useState} from "react";
 import Indicator from "@/components/indicator";
 import {clientError, connectionError} from "@/api/utils";
 import {Errors} from "@/components/errors";
-import axios from "@/api/axios";
 import useFetch from "@/hooks/useFetch";
+import {useRouter} from "next/navigation";
 
 export default function Signup() {
     const [payload, setPayload] = useState(null);
-    const [loginPayload, setLoginPayload] = useState(null);
     const {success, isLoading, errors} = useFetch("POST", "/account/register/", payload);
-    const {loginSuccess, loginResult, loginIsLoading, loginErrors} = useFetch("POST", "/accounts/jwt/", loginPayload);
+    const router = useRouter();
 
     async function handleSubmit(event) {
         event.preventDefault();
@@ -32,25 +31,19 @@ export default function Signup() {
             }
         });
     }
-    if (success && !loginSuccess) {
-        setLoginPayload({email: payload.email, password: payload.password});
-    }
-    if (loginSuccess) {
-        axios.defaults.headers.common = {'Authorization': `Bearer ${loginResult.access}`};
-        localStorage.setItem("access", loginResult.access);
-        localStorage.setItem("refresh", loginResult.refresh);
-        router.push("/feed");
+    if (success) {
+        router.push("/welcome");
     }
 
     return (
         <main className="min-h-screen bg-white flex">
             <div className="m-auto w-80 space-y-5 flex flex-col">
-                <Errors errors={[errors.concat(loginErrors)]} />
+                <Errors errors={errors} />
                 <form onSubmit={handleSubmit} className="flex flex-col space-y-5">
                     <input className="h-10 border-2 p-0.5 rounded-sm" id="email" type="email" placeholder="Email" required/>
                     <input className="h-10 border-2 p-0.5 rounded-sm" minLength="8" id="password" type="password" placeholder="Password" required/>
                     <input className="h-10 border-2 p-0.5 rounded-sm" minLength="8" id="confirmPassword" type="password" placeholder="Confirm Password" required/>
-                    {isLoading || loginIsLoading ? <Indicator className="p-1.5 m-auto h-10 w-20 rounded-sm bg-std-blue-hover"/> : <button className="m-auto h-10 w-20 rounded-sm bg-std-blue hover:bg-std-blue-hover text-white font-medium text-base" type="submit" value="Signup">Sign Up</button>}
+                    {isLoading ? <Indicator className="p-1.5 m-auto h-10 w-20 rounded-sm bg-std-blue-hover"/> : <button className="m-auto h-10 w-20 rounded-sm bg-std-blue hover:bg-std-blue-hover text-white font-medium text-base" type="submit" value="Signup">Sign Up</button>}
                 </form>
             </div>
         </main>
---
+++ app/welcome/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Welcome() {
+
+    return (
+        <main className="bg-white min-h-screen">
+            <h1>We have sent you an email containing a link to verify your account</h1>
+            <p>Please verify your email before you can proceed</p>
+        </main>
+    )
+}
