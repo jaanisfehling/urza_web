a83ec638f57323a9b012b3733ee1386a95a50eb8
jaanisfehling <jaanisfehling@gmail.com>
2023-06-12 19:32:52 +0200

    welcome page functional

--- app/welcome/page.js
+++ app/welcome/page.js
@@ -3,17 +3,19 @@
 import useFetch from "@/hooks/useFetch";
 import Button from "@/components/button";
 import Errors from "@/components/errors";
+import { useState } from "react";
 
 export default function Welcome() {
-    const {errors, isLoading} = useFetch("POST", "/account/users/resend_activation/", {email: sessionStorage.getItem("email")});
+    const [payload, setPayload] = useState();
+    const {errors, isLoading} = useFetch("POST", "/account/users/resend_activation/", payload);
 
     return (
         <div className="bg-white min-h-screen flex">
             <div className="m-auto w-80 space-y-5 flex flex-col">
+                <Errors errors={errors} />
                 <p>We have sent you an email containing a link to verify your account</p>
                 <p>Please verify your email before you can proceed</p>
-                <Errors errors={errors} />
-                <Button text="Resend mail" isLoading={isLoading} onClick={() => {}}/>
+                <Button text="Resend mail" isLoading={isLoading} onClick={() => {setPayload({email: sessionStorage.getItem("email")})}}/>
             </div>
         </div>
     )
