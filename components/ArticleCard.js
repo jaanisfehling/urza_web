3ccf2375018fa42e02fe7d97823a4c4ffdfc4bea
jaanisfehling <jaanisfehling@gmail.com>
2023-06-24 00:31:14 +0200

    class names order change

--- components/ArticleCard.js
+++ components/ArticleCard.js
@@ -7,8 +7,7 @@ export default function ArticleCard({className, article, isSelected, onClick}) {
     }
 
     return (
-        <button className={`mx-2 my-1 p-2.5 box-border rounded-sm border-gray-400 dark:border-gray-700 flex flex-col space-y-2 ${className}`}
-                onClick={onClick}>
+        <button className={`flex flex-col space-y-2 mx-2 my-1 p-2.5 box-border rounded-sm border-gray-400 dark:border-gray-700 ${className}`} onClick={onClick}>
             <b>{article?.title}</b>
             <span>{new Date(article?.date_time).toLocaleString()}</span>
         </button>
