5d24ff9183098273859580bf503742b585e27bee
jaanisfehling <jaanisfehling@gmail.com>
2023-07-25 00:00:47 +0200

    highlight current page in navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import dynamic from 'next/dynamic'
 import {ReactNode} from "react";
+import Providers from "@/context_providers/Providers";
 
 const DynamicNavbar = dynamic(() => import("@/components/Navbar"), {ssr: false})
 const inter = Inter({ subsets: ['latin'] })
@@ -15,12 +16,14 @@ export default function RootLayout({ children }: {children?: ReactNode}) {
     return (
         <html lang="en">
             <body className={inter.className + ""}>
-                <div className="flex flex-col min-h-screen bg-white dark:bg-gray-900">
-                    <div className="h-14">
-                        <DynamicNavbar/>
+                <Providers>
+                    <div className="flex flex-col min-h-screen bg-white dark:bg-gray-900">
+                        <div className="h-14">
+                            <DynamicNavbar/>
+                        </div>
+                        {children}
                     </div>
-                    {children}
-                </div>
+                </Providers>
             </body>
         </html>
     )
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import Link from "next/link";
 import {refreshTokenValid} from "@/api/utils";
 import {useState} from "react";
+import Link from "next/link";
 
 export default function Navbar() {
     const [showSidebar, setShowSidebar] = useState(false);
+    const [currentPage, setPage] = useState("");
 
-    let links;
-    if (typeof document !== "undefined" && refreshTokenValid()) {
+    let links: {link: string, text: string}[];
+    if (refreshTokenValid()) {
         links = [
             {link: "/feed", text: "Urza"},
             {link: "/dashboard", text: "Dashboard"},
@@ -23,23 +24,23 @@ export default function Navbar() {
     }
 
     return (
-        <div>
+        <>
             <div className="sticky flex top-0 h-14 w-full px-4 items-center justify-between border-b-2 border-gray-700 dark:border-gray-700 bg-white dark:bg-gray-900">
-                <Link href={links[0].link} className="text-2xl font-normal text-black dark:text-white">{links[0].text}</Link>
+                <Link href={links[0].link} onClick={() => {setPage(links[0].link)}} className="text-2xl font-normal text-black dark:text-white">{links[0].text}</Link>
                 <div className="relative flex">
-                    <button onClick={() => setShowSidebar(!showSidebar)} className="absolute top-2 right-0 z-10 lg:invisible px-2.5 rounded-sm bg-white dark:bg-gray-900 text-black dark:text-white text-2xl">{showSidebar ? "✖" : "☰"}</button>
+                    <Link href={links[0].link} onClick={() => setShowSidebar(!showSidebar)} className="absolute top-2 right-0 z-10 lg:invisible px-2.5 rounded-sm bg-white dark:bg-gray-900 text-black dark:text-white text-2xl">{showSidebar ? "✖" : "☰"}</Link>
                     <div className="flex space-x-2 invisible lg:visible">
-                        <Link href={links[1].link} className="flex items-center h-8 px-2.5 m-2 text-black dark:text-white font-normal text-base">{links[1].text}</Link>
-                        <Link href={links[2].link} className="flex items-center h-8 px-2.5 m-2 text-black dark:text-white font-normal text-base">{links[2].text}</Link>
+                        <Link href={links[1].link} onClick={() => {setPage(links[1].link)}} className={`flex items-center h-8 px-2.5 m-2 text-black dark:text-white font-normal ${currentPage == links[1].link ? "font-bold" : "font-normal"}`}>{links[1].text}</Link>
+                        <Link href={links[2].link} onClick={() => {setPage(links[2].link)}} className={`flex items-center h-8 px-2.5 m-2 text-black dark:text-white font-normal ${currentPage == links[2].link  ? "font-bold" : "font-normal"}`}>{links[2].text}</Link>
                     </div>
                 </div>
             </div>
             {showSidebar &&
             <div className="flex flex-col space-y-5 fixed h-full w-full z-40 pt-5 overflow-y-auto bg-white dark:bg-gray-900">
-                <Link href={links[1].link} className="flex items-center ml-8 text-std text-black dark:text-white font-normal text-base">{links[1].text}</Link>
-                <Link href={links[2].link} className="flex items-center ml-8 text-std text-black dark:text-white font-normal text-base">{links[2].text}</Link>
+                <Link href={links[1].link} onClick={() => {setPage(links[1].link); setShowSidebar(false)}} className={`flex items-center ml-8 text-std text-black dark:text-white ${currentPage == links[1].link  ? "font-bold" : "font-normal"}`}>{links[1].text}</Link>
+                <Link href={links[2].link} onClick={() => {setPage(links[2].link); setShowSidebar(false)}} className={`flex items-center ml-8 text-std text-black dark:text-white ${currentPage == links[2].link  ? "font-bold" : "font-normal"}`}>{links[2].text}</Link>
             </div>
             }
-        </div>
+        </>
     )
 }
