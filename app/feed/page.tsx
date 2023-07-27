9e0d12b819408d992103f95cbb1249d06a8797fd
jaanisfehling <jaanisfehling@gmail.com>
2023-07-27 02:10:37 +0200

    fix react-grid-layout

--- app/feed/page.tsx
+++ app/feed/page.tsx
@@ -2,7 +2,7 @@
 
 import "/node_modules/react-grid-layout/css/styles.css";
 import "/node_modules/react-resizable/css/styles.css";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import useFetch from "@/hooks/useFetch";
 import Errors from "@/components/Errors";
 import ArticleView from "@/components/ArticleView";
@@ -44,20 +44,23 @@ export default function Feed() {
     }, [articleList]);
 
     return (
-        <div className="">
-            <Errors className="top-14" errors={["erro", ...wsErrors||[], ...errors||[]]}/>
+        <>
+            <Errors className="top-14" errors={[...errors||[], ...wsErrors||[]]}/>
             <ResponsiveGridLayout className="layout"
                                   cols={{ lg: 5, md: 5, sm: 3, xs: 1, xxs: 1 }}
+                                  rowHeight={(window.innerHeight-126)/6}
                                   breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
                                   isResizable={false}
                                   isBounded={true}>
-                <div key="a" data-grid={{ x: 0, y: 0, w: 1, h: 2 }}>
+                <div key="a" data-grid={{ x: 0, y: 0, w: 1, h: 6 }}>
                     {articleList && articleList?.length > 0 && <ArticleList articleList={[...messages||[], ...articleList||[]]} selectedArticle={selectedArticle} setSelectedArticle={setSelectedArticle} onLoadMoreClick={() => {setNewsUrl(result ? result?.next : "")}}/>}
                 </div>
-                <div key="b" data-grid={{ x: 1, y: 0, w: 2, h: 5 }}>
+                <div key="b" data-grid={{ x: 1, y: 0, w: 2, h: 6 }}>
                     {selectedArticle && <ArticleView className="pt-4 px-10 h-full w-full overflow-auto rounded-sm border-2 border-gray-400 dark:border-gray-700" article={selectedArticle}/>}
                 </div>
+                <div key="c" data-grid={{ x: 3, y: 0, w: 2, h: 3 }}>
+                </div>
             </ResponsiveGridLayout>
-        </div>
+        </>
     )
 }
