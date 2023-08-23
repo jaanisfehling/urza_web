11ba8a0db7909b3e7254c993f7045c31e026db36
jaanisfehling <jaanisfehling@gmail.com>
2023-08-23 18:34:26 +0200

    add feed to navbar even when user is logged out

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,19 +16,17 @@ export default function Navbar() {
     let links: {link: string, text: string}[];
     if (loggedInContext?.isLoggedIn) {
         links = [
-            {link: "/feed", text: "Urza"},
+            {link: "/feed", text: "Feed"},
             {link: "/account", text: "Account"},
-            {link: "/feed", text: "Feed"}
         ]
     } else if (loggedInContext?.isLoggedIn == false) {
         links = [
-            {link: "/", text: "Urza"},
+            {link: "/feed", text: "Feed"},
             {link: "/login", text: "Login"},
             {link: "/signup", text: "Sign Up"}
         ]
     } else {
         links = [
-            {link: "/", text: ""},
             {link: "/", text: ""},
             {link: "/", text: ""}
         ]
@@ -37,19 +35,21 @@ export default function Navbar() {
     return (
         <>
             <div className="flex h-14 px-4 items-center justify-between border-b-2 border-gray-700 dark:border-gray-700 bg-white dark:bg-gray-900">
-                <Link href={links[0].link} className="text-2xl font-normal">{links[0].text}</Link>
+                <Link href={loggedInContext?.isLoggedIn ? "/feed" : "/"} className="text-2xl font-normal">Urza</Link>
                 <div className="relative flex">
                     <button onClick={() => setShowSidebar(!showSidebar)} className="absolute top-2 right-0 z-20 md:invisible px-2.5 rounded-sm text-2xl">{showSidebar ? "✖" : "☰"}</button>
                     <div className="flex space-x-2 invisible md:visible">
-                        <Link href={links[1].link} className="flex items-center h-8 px-2.5 m-2">{links[1].text}</Link>
-                        <Link href={links[2].link} className="flex items-center h-8 px-2.5 m-2">{links[2].text}</Link>
+                        {links.map((e, i) =>
+                            <Link key={i} href={e.link} className="flex items-center h-8 px-2.5 m-2">{e.text}</Link>)
+                        }
                     </div>
                 </div>
             </div>
             {showSidebar &&
             <div className="flex flex-col space-y-5 fixed h-full w-full z-40 pt-5 overflow-y-auto bg-white dark:bg-gray-900">
-                <Link href={links[1].link} onClick={() => setShowSidebar(false)} className="flex items-center ml-8 text-std">{links[1].text}</Link>
-                <Link href={links[2].link} onClick={() => setShowSidebar(false)} className="flex items-center ml-8 text-std">{links[2].text}</Link>
+                {links.map((e, i) =>
+                    <Link key={i} href={links[1].link} onClick={() => setShowSidebar(false)} className="flex items-center ml-8 text-std">{links[1].text}</Link>)
+                }
             </div>
             }
         </>
